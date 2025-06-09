8271acd92536b30194ad9b0bf243a731e341067a
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-09 23:27:04 +0530

    add eqpment data and images

---
+++ workout_planner_app/lib/data/equipment_data.dart
@@ -0,0 +1,100 @@
+import 'package:workout_planner_app/models/equipment_model.dart';
+
+class EquipmentData {
+  final List<Equipment> equipmentList = [
+    Equipment(
+      id: 0,
+      equipmentName: "Dumbbells",
+      equipmentDes: "A pair of dumbbells for strength training exercises.",
+      equipmentImgURL: "assets/equipments/calendar.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 1,
+      equipmentName: "Resistance Bands",
+      equipmentDes: "Elastic bands used for resistance exercises.",
+      equipmentImgURL: "assets/equipments/checklist.png",
+      noOfMin: 20,
+      noOfCalaris: 15,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 2,
+      equipmentName: "Yoga Mat",
+      equipmentDes: "A mat for practicing yoga and floor exercises.",
+      equipmentImgURL: "assets/equipments/dumbbell.png",
+      noOfMin: 60,
+      noOfCalaris: 30,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 3,
+      equipmentName: "Jump Rope",
+      equipmentDes:
+          "A rope used for cardiovascular exercises and coordination training.",
+      equipmentImgURL: "assets/equipments/dumbbells.png",
+      noOfMin: 30,
+      noOfCalaris: 200,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 4,
+      equipmentName: "Kettlebells",
+      equipmentDes:
+          "Weights with a handle used for strength, endurance, and flexibility training.",
+      equipmentImgURL: "assets/equipments/facial-roller.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 5,
+      equipmentName: "Exercise Ball",
+      equipmentDes: "A pair of dumbbells for strength training exercises.",
+      equipmentImgURL: "assets/equipments/stationary-bike.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 6,
+      equipmentName: "Pull-up Bar",
+      equipmentDes: "A pair of dumbbells for strength training exercises.",
+      equipmentImgURL: "assets/equipments/treadmill.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 7,
+      equipmentName: "Medicine Ball",
+      equipmentDes:
+          "A heavy ball used for strength and coordination exercises.",
+      equipmentImgURL: "assets/equipments/facial-roller.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 8,
+      equipmentName: "Bench",
+      equipmentDes: "A stable surface for various strength training exercises.",
+      equipmentImgURL: "assets/equipments/dumbbells.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+    Equipment(
+      id: 9,
+      equipmentName: "Barbell",
+      equipmentDes:
+          "A long metal bar with weights attached to each end, used for resistance training.",
+      equipmentImgURL: "assets/equipments/skipping-rope.png",
+      noOfMin: 30,
+      noOfCalaris: 2,
+      handOvered: false,
+    ),
+  ];
+}
