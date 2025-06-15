5dc8ca782e85f683a3c31e437b3f7ed8685f6039
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-15 19:32:46 +0530

    create add exsasise wight

---
+++ workout_planner_app/lib/widgets/addExerciseCard.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AddexErciseCard extends StatefulWidget {
+  const AddexErciseCard({super.key});
+
+  @override
+  State<AddexErciseCard> createState() => _AddexErciseCardState();
+}
+
+class _AddexErciseCardState extends State<AddexErciseCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
