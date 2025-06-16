00984a4ab3861783f59e9a165ebd903adbaea465
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-16 20:06:42 +0530

    add fav page fav exsasices

--- workout_planner_app/lib/pages/favorite_page/favorite_page.dart
+++ workout_planner_app/lib/pages/favorite_page/favorite_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:workout_planner_app/const_styles/colors.dart';
 import 'package:workout_planner_app/const_styles/responsive.dart';
+import 'package:workout_planner_app/data/user_data.dart';
+import 'package:workout_planner_app/models/exercise_model.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -9,6 +12,9 @@ class FavoritePage extends StatefulWidget {
 }
 
 class _FavoritePageState extends State<FavoritePage> {
+
+  final userData = user;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +22,73 @@ class _FavoritePageState extends State<FavoritePage> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(HDefaultPadding),
-            child: Column(children: [
-              Center(
-              child: 
-              Text("Favorite Page")
-              )]),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Here are all your favorited Workouts",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    color: HGradientTopColor,
+                  ),
+                ),
+                SizedBox(height: 20),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: HDefaultPadding,
+                    mainAxisSpacing: HDefaultPadding,
+                  ),
+                  itemCount: userData.favExerciseList.length,
+                  itemBuilder: (context, index) {
+                    Exercise favExercise = userData.favExerciseList[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(HDefaultPadding),
+                      child: Card(
+                        child: Column(
+                          children: [
+                            Text(
+                              favExercise.exerciseName,
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w700,
+                                color: HMainColor,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Image.asset(
+                              favExercise.exerciseImgURL,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "${favExercise.numOfMin} Min Workout",
+                              style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.normal,
+                                color: HMainPinkColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                Text(
+                  "Here are all your favorited equipment",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: HGradientTopColor,
+                  ),
+                ),
+                SizedBox(height: 20),
+              ]),
           ),
         ),
       ),
